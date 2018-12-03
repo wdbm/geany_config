a840fc679f0a2794fb862ea2c8d4c0d21a2dc1c5
Will Breaden Madden <wbm@protonmail.ch>
2018-12-03 14:22:03 +0000

    tidy

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
+echo "configure Geany"
 rm -rf ~/.config/geany
 sudo mkdir -p /usr/share/geany
-
 cp -R dot_config_geany ~/.config/geany
 sudo cp usr_share_geany/filetypes.common /usr/share/geany/
